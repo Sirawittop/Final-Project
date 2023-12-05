bf3d8e209d1146ee6b6f35cb677318cde2ba9e41
Sirawit Kamchoom <sirawitkc@gmail.com>
2023-12-05 18:04:36 +0700

    clear

--- main.go
+++ main.go
@@ -3,21 +3,12 @@ package main
 import (
 	"fmt"
 
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func main() {
-	dsn := "top:1234@tcp(127.0.0.1:8889)/nurse_scheduler?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect to the database")
-	}
-	//show struct in database models.go hospital
-	var Hospital Hospital
-	DB.First(&Hospital)
-	fmt.Println(Hospital)
+	fmt.Println("func")
+
 }
