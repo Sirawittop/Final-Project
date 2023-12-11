bbf16f690304dd49bfd2bfe4921b51a4a129fd03
Sirawit Kamchoom <sirawitkc@gmail.com>
2023-12-11 14:17:02 +0700

    make db gorm

--- main.go
+++ main.go
@@ -11,34 +11,31 @@ import (
 // ... (your existing code)
 type plan_types struct {
 	gorm.Model
-	Plan_Type_ID int
-	TypeName     string
-	Morning      bool
-	Afternoon    bool
-	Night        bool
-	X            bool
-	V            bool
-	Nleave       bool
-	C            bool
-	Part         bool
-	OtM          bool
-	OtA          bool
-	OtN          bool
-	StatusS      bool
-	StatusOt12   bool
-	StatusOt8    bool
+	TypeName   string
+	Morning    bool
+	Afternoon  bool
+	Night      bool
+	X          bool
+	V          bool
+	Nleave     bool
+	C          bool
+	Part       bool
+	OtM        bool
+	OtA        bool
+	OtN        bool
+	StatusS    bool
+	StatusOt12 bool
+	StatusOt8  bool
 }
 
 type hospitals struct {
 	gorm.Model
-	HospitalCode  string
-	hospital_name string
+	Hospital_name string
 }
 
 type ward_data struct {
 	gorm.Model
-	Ward_ID      int
-	HospitalCode string
+	HospitalCode hospitals `gorm:"foreignKey:ID"`
 	WardName     string
 }
 
@@ -56,7 +53,7 @@ type nurse_data struct {
 	Employment_Year           int
 	Hospital_ID               int
 	Salary                    int
-	gender                    string
+	Gender                    string
 	Phone_Number              string
 	Number_Children           int
 	MaritalS_ID               int
@@ -72,48 +69,45 @@ type nurse_data struct {
 	Subdistrict               string
 }
 
-type plan struct {
+type NursingPlan struct {
 	gorm.Model
-	plan_types plan_types
-	hospitals  hospitals
-	ward_data  ward_data
-	nurse_data nurse_data
-	day1       plan_types
-	day2       plan_types
-	day3       plan_types
-	day4       plan_types
-	day5       plan_types
-	day6       plan_types
-	day7       plan_types
-	day8       plan_types
-	day9       plan_types
-	day10      plan_types
-	day11      plan_types
-	day12      plan_types
-	day13      plan_types
-	day14      plan_types
-	day15      plan_types
-	day16      plan_types
-	day17      plan_types
-	day18      plan_types
-	day19      plan_types
-	day20      plan_types
-	day21      plan_types
-	day22      plan_types
-	day23      plan_types
-	day24      plan_types
-	day25      plan_types
-	day26      plan_types
-	day27      plan_types
-	day28      plan_types
-	day29      plan_types
-	day30      plan_types
-	day31      plan_types
+	Nures nurse_data `gorm:"foreignKey:ID"`
+	Day1  plan_types `gorm:"foreignKey:ID"`
+	Day2  plan_types `gorm:"foreignKey:ID"`
+	Day3  plan_types `gorm:"foreignKey:ID"`
+	Day4  plan_types `gorm:"foreignKey:ID"`
+	Day5  plan_types `gorm:"foreignKey:ID"`
+	Day6  plan_types `gorm:"foreignKey:ID"`
+	Day7  plan_types `gorm:"foreignKey:ID"`
+	Day8  plan_types `gorm:"foreignKey:ID"`
+	Day9  plan_types `gorm:"foreignKey:ID"`
+	Day10 plan_types `gorm:"foreignKey:ID"`
+	Day11 plan_types `gorm:"foreignKey:ID"`
+	Day12 plan_types `gorm:"foreignKey:ID"`
+	Day13 plan_types `gorm:"foreignKey:ID"`
+	Day14 plan_types `gorm:"foreignKey:ID"`
+	Day15 plan_types `gorm:"foreignKey:ID"`
+	Day16 plan_types `gorm:"foreignKey:ID"`
+	Day17 plan_types `gorm:"foreignKey:ID"`
+	Day18 plan_types `gorm:"foreignKey:ID"`
+	Day19 plan_types `gorm:"foreignKey:ID"`
+	Day20 plan_types `gorm:"foreignKey:ID"`
+	Day21 plan_types `gorm:"foreignKey:ID"`
+	Day22 plan_types `gorm:"foreignKey:ID"`
+	Day23 plan_types `gorm:"foreignKey:ID"`
+	Day24 plan_types `gorm:"foreignKey:ID"`
+	Day25 plan_types `gorm:"foreignKey:ID"`
+	Day26 plan_types `gorm:"foreignKey:ID"`
+	Day27 plan_types `gorm:"foreignKey:ID"`
+	Day28 plan_types `gorm:"foreignKey:ID"`
+	Day29 plan_types `gorm:"foreignKey:ID"`
+	Day30 plan_types `gorm:"foreignKey:ID"`
+	Day31 plan_types `gorm:"foreignKey:ID"`
 }
 
 func main() {
 	// connect to the database
-	dsn := "top:1234@tcp(127.0.0.1:8889)/ORM_Data?parseTime=true"
+	dsn := "top:1234@tcp(127.0.0.1:8889)/Nurse_Shift_scheduling?parseTime=true"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Error connecting to the database:", err)
@@ -121,11 +115,35 @@ func main() {
 	}
 
 	// migrate
-	db.AutoMigrate(&plan{})
+	db.AutoMigrate(&NursingPlan{}, &ward_data{})
 
-	// query
-	var plan_types []plan
-	db.Find(&plan_types)
-	fmt.Println(plan_types)
+	// Handle HTTP requests
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	// Query data from the database
+	// 	var nursingPlans []NursingPlan
+	// 	db.Find(&nursingPlans)
 
+	// 	// Render HTML template with the data
+	// 	renderTemplate(w, nursingPlans)
+	// })
+
+	// // Start the HTTP server
+	// http.ListenAndServe(":5555", nil)
 }
+
+// Render HTML template with data
+// func renderTemplate(w http.ResponseWriter, nursingPlans []NursingPlan) {
+// 	// Read the HTML template from the file
+// 	tmpl, err := template.ParseFiles("template.html")
+// 	if err != nil {
+// 		http.Error(w, "Error parsing template", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	// Execute the template with the data and write to the response
+// 	err = tmpl.Execute(w, nursingPlans)
+// 	if err != nil {
+// 		http.Error(w, "Error executing template", http.StatusInternalServerError)
+// 		return
+// 	}
+// }
