4f5138a667b68524d35c2c09900470940c85f39d
Sirawit Kamchoom <sirawitkc@gmail.com>
2023-12-15 18:21:11 +0700

    Make Function Map Plan, Plantype

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"reflect"
-
 	"nurse_shift/model"
 
 	"gorm.io/driver/mysql"
@@ -12,19 +9,14 @@ import (
 )
 
 var plans []model.Plans
-var plantypes []model.Plantypes
-var nurses []model.Nurses
-var wards []model.Wards
-var hospitals []model.Hospitals
+var planTypes []model.Plantypes
 
-// main function to make list
-func makeList(plan model.Plans) (typeplaninplan []int) {
-	val := reflect.ValueOf(plan)
-	for i := 2; i < val.NumField(); i++ {
-		fieldValue := val.Field(i)
-		typeplaninplan = append(typeplaninplan, int(fieldValue.Int()))
+func mapPlanwithPlantype(plans []model.Plans, planTypes []model.Plantypes) map[int]string {
+	planNumberMapping := make(map[int]string)
+	for _, planType := range planTypes {
+		planNumberMapping[planType.ID] = planType.TypeName
 	}
-	return typeplaninplan
+	return planNumberMapping
 }
 
 func main() {
@@ -34,16 +26,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error connecting to the database:", err)
 	}
-	// Migrate the schema
-	db.Find(&plantypes)
-	db.Find(&plans)
-	db.Find(&nurses)
-	db.Find(&wards)
-	db.Find(&hospitals)
 
-	for _, plan := range plans {
-		result := makeList(plan)
-		fmt.Println(result)
-	}
+	// Migrate the schema
+	db.Find(&plans)
+	db.Find(&planTypes)
+
+	// call mapPlanwithPlantype function
 
 }
