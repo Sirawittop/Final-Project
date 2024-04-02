2388dbb8fb03c7a46ce6062fa28f053b785a1b42
Sirawit Kamchoom <sirawitkc@gmail.com>
2024-04-03 03:12:14 +0700

    update id nurse

--- main.go
+++ main.go
@@ -2,50 +2,97 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
-	"net/http"
 	"nurse_shift/model"
 	"reflect"
-	"strings"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var plans []model.Plans
+var planTypes []model.Plantypes
+var hospitals []model.Hospitals
+var wards []model.Wards
+var nurses []model.User
+var OTs []model.OTs
+
+// func calculateOT(nurseIndex int, planWithType []string, OTs []model.OTs, plans []model.Plans) int {
+// 	planWithTypeForNurse := getPlanWithTypeForNurse(nurseIndex, planWithType)
+// 	otmor := 0
+// 	otaft := 0
+// 	otnin := 0
+
+// 	// Ot คิดยังไงเมื่อ OT/OT เช่น rด/rช คิดเป็น OT 1 หรือ OT 2
+// 	for _, planType := range planWithTypeForNurse {
+// 		if planType == "rด/บ" || planType == "x/ด" || planType == "rด/rบ" || planType == "rด/rช" {
+// 			otnin++
+// 		}
+// 		if planType == "rช/บ" || planType == "ด/rช" || planType == "x/ช" || planType == "rช/rบ" || planType == "rด/rช" {
+// 			otmor++
+// 		}
+// 		if planType == "ช/rบ" || planType == "ด/rบ" || planType == "rด/ช" || planType == "x/บ" || planType == "rช/rบ" || planType == "rด/rบ" {
+// 			otaft++
+// 		}
+// 	}
+// 	if plans[nurseIndex].OT == 1 {
+// 		return (OTs[0].Morning * otmor) + (OTs[0].Afternoon * otaft) + (OTs[0].Night * otnin)
+// 	} else if plans[nurseIndex].OT == 2 {
+// 		return (OTs[1].Morning * otmor) + (OTs[1].Afternoon * otaft) + (OTs[1].Night * otnin)
+// 	} else if plans[nurseIndex].OT == 3 {
+// 		return (OTs[2].Morning * otmor) + (OTs[2].Afternoon * otaft) + (OTs[2].Night * otnin)
+// 	}
+// 	return 0
+// }
+// func splitPlanTypeName(planTypeName string) string {
+// 	parts := strings.Split(planTypeName, "")
+// 	if parts[0] == "r" && parts[1] == "ด" && parts[3] == "ช" {
+// 		return "red-ด black-ช"
+// 	} else if parts[0] == "r" && parts[1] == "ด" && parts[3] == "บ" {
+// 		return "red-ด black-บ"
+// 	} else if parts[0] == "r" && parts[1] == "ช" && parts[3] == "บ" {
+// 		return "red-ช black-บ"
+// 	} else if parts[2] == "r" && parts[0] == "ด" && parts[3] == "ช" {
+// 		return "black-ด red-ช"
+// 	} else if parts[2] == "r" && parts[0] == "ด" && parts[3] == "บ" {
+// 		return "black-ด red-บ"
+// 	} else if parts[2] == "r" && parts[0] == "ช" && parts[3] == "บ" {
+// 		return "black-ช red-บ"
+// 	} else if parts[0] == "r" && parts[1] == "ด" && parts[3] == "r" && parts[4] == "ช" {
+// 		return "red-ด red-ช"
+// 	} else if parts[0] == "r" && parts[1] == "ด" && parts[3] == "r" && parts[4] == "บ" {
+// 		return "red-ด red-บ"
+// 	} else if parts[0] == "r" && parts[1] == "ช" && parts[3] == "r" && parts[4] == "บ" {
+// 		return "red-ช red-บ"
+// 	}
+// 	return ""
+// }
+
 func mapPlanwithPlantype(plans []model.Plans, planTypes []model.Plantypes) map[int]string {
 	planNumberMapping := make(map[int]string)
 	for _, planType := range planTypes {
-		planNumberMapping[planType.ID] = planType.TypeName
+		planNumberMapping[int(planType.ID)] = planType.Type
 	}
 	return planNumberMapping
 }
 
-func generatePlanWithTypeMapping(plans []model.Plans, planNumberMapping map[int]string) []string {
-	var planWithType []string
-	for i := 0; i < len(plans); i++ {
+func generatePlanWithTypeMapping(plans []model.Plans, planNumberMapping map[int]string) map[uint][]string {
+	result := make(map[uint][]string)
+	for _, plan := range plans {
+		nurseID := plan.Nurse_id
+		var planWithType []string
 		for j := 1; j <= 31; j++ {
 			dayField := fmt.Sprintf("Day%d", j)
-			field := reflect.ValueOf(plans[i]).FieldByName(dayField)
-			planType := int(field.Int())
+			field := reflect.ValueOf(plan).FieldByName(dayField)
+			planType := int(field.Uint())
 			planWithType = append(planWithType, planNumberMapping[planType])
 		}
+		result[nurseID] = planWithType
 	}
-	return planWithType
+	return result
 }
 
-func getPlanWithTypeForNurse(nurseIndex int, planWithType []string) []string {
-	start := nurseIndex * 31
-	end := start + 31
-	if end > len(planWithType) {
-		end = len(planWithType)
-	}
-	return planWithType[start:end]
-}
-
-func calculateNursesShift(nurseIndex int, planWithType []string) []int {
-	planWithTypeForNurse := getPlanWithTypeForNurse(nurseIndex, planWithType)
+func calculateNursesShift(planWithType []string) []int {
 	var calnurseshift []int
 	mor := 0
 	aft := 0
@@ -56,32 +103,32 @@ func calculateNursesShift(nurseIndex int, planWithType []string) []int {
 	lev := 0
 	cav := 0
 	cat := 0
-	for _, planType := range planWithTypeForNurse {
-		if planType == "ช" || planType == "T" || planType == "ช/rบ" || planType == "rด/ช" || planType == "ช2" {
+	for _, PlanID := range planWithType {
+		if PlanID == "ช" || PlanID == "ช/rบ" || PlanID == "rด/ช" || PlanID == "ช2" {
 			mor++
 		}
-		if planType == "บ" || planType == "rช/บ" || planType == "rด/บ" {
+		if PlanID == "บ" || PlanID == "rช/บ" || PlanID == "rด/บ" {
 			aft++
 		}
-		if planType == "ด" || planType == "ด/rบ" || planType == "ด/rช" {
+		if PlanID == "ด" || PlanID == "ด/rบ" || PlanID == "ด/rช" {
 			nin++
 		}
-		if planType == "X" {
+		if PlanID == "X" {
 			off++
 		}
-		if planType == "C" {
+		if PlanID == "C" {
 			onc++
 		}
-		if planType == "rช/บ" || planType == "ช/rบ" || planType == "ด/rบ" || planType == "rด/บ" || planType == "ด/rช" || planType == "rด/ช" || planType == "x/ช" || planType == "x/บ" || planType == "x/ด" || planType == "rช/rบ" || planType == "rด/rบ" || planType == "rด/rช" {
+		if PlanID == "rช/บ" || PlanID == "ช/rบ" || PlanID == "ด/rบ" || PlanID == "rด/บ" || PlanID == "ด/rช" || PlanID == "rด/ช" || PlanID == "x/ช" || PlanID == "x/บ" || PlanID == "x/ด" || PlanID == "rช/rบ" || PlanID == "rด/rบ" || PlanID == "rด/rช" {
 			otv++
 		}
-		if planType == "ล" {
+		if PlanID == "ล" {
 			lev++
 		}
-		if planType == "v" {
+		if PlanID == "V" {
 			cav++
 		}
-		if planType == "T" {
+		if PlanID == "T" {
 			cat++
 		}
 	}
@@ -89,72 +136,23 @@ func calculateNursesShift(nurseIndex int, planWithType []string) []int {
 	return calnurseshift
 }
 
-func splitPlanTypeName(planTypeName string) string {
-	parts := strings.Split(planTypeName, "")
-	if parts[0] == "r" && parts[1] == "ด" && parts[3] == "ช" {
-		return "red-ด black-ช"
-	} else if parts[0] == "r" && parts[1] == "ด" && parts[3] == "บ" {
-		return "red-ด black-บ"
-	} else if parts[0] == "r" && parts[1] == "ช" && parts[3] == "บ" {
-		return "red-ช black-บ"
-	} else if parts[2] == "r" && parts[0] == "ด" && parts[3] == "ช" {
-		return "black-ด red-ช"
-	} else if parts[2] == "r" && parts[0] == "ด" && parts[3] == "บ" {
-		return "black-ด red-บ"
-	} else if parts[2] == "r" && parts[0] == "ช" && parts[3] == "บ" {
-		return "black-ช red-บ"
-	} else if parts[0] == "r" && parts[1] == "ด" && parts[3] == "r" && parts[4] == "ช" {
-		return "red-ด red-ช"
-	} else if parts[0] == "r" && parts[1] == "ด" && parts[3] == "r" && parts[4] == "บ" {
-		return "red-ด red-บ"
-	} else if parts[0] == "r" && parts[1] == "ช" && parts[3] == "r" && parts[4] == "บ" {
-		return "red-ช red-บ"
+func mapNurseUser(nurse []model.User) map[uint]string {
+	nurseMapping := make(map[uint]string)
+	for _, nurseUser := range nurse {
+		nurseMapping[nurseUser.ID] = nurseUser.Firstname + " " + nurseUser.Lastname
 	}
-	return ""
+
+	return nurseMapping
 }
 
-func calculateOT(nurseIndex int, planWithType []string, OTs []model.OT, plans []model.Plans) int {
-	planWithTypeForNurse := getPlanWithTypeForNurse(nurseIndex, planWithType)
-	otmor := 0
-	otaft := 0
-	otnin := 0
-
-	// Ot คิดยังไงเมื่อ OT/OT เช่น rด/rช คิดเป็น OT 1 หรือ OT 2
-	for _, planType := range planWithTypeForNurse {
-		if planType == "rด/บ" || planType == "x/ด" || planType == "rด/rบ" || planType == "rด/rช" {
-			otnin++
-		}
-		if planType == "rช/บ" || planType == "ด/rช" || planType == "x/ช" || planType == "rช/rบ" || planType == "rด/rช" {
-			otmor++
-		}
-		if planType == "ช/rบ" || planType == "ด/rบ" || planType == "rด/ช" || planType == "x/บ" || planType == "rช/rบ" || planType == "rด/rบ" {
-			otaft++
-		}
-	}
-	if plans[nurseIndex].OT == 1 {
-		return (OTs[0].Morning * otmor) + (OTs[0].Afternoon * otaft) + (OTs[0].Night * otnin)
-	} else if plans[nurseIndex].OT == 2 {
-		return (OTs[1].Morning * otmor) + (OTs[1].Afternoon * otaft) + (OTs[1].Night * otnin)
-	} else if plans[nurseIndex].OT == 3 {
-		return (OTs[2].Morning * otmor) + (OTs[2].Afternoon * otaft) + (OTs[2].Night * otnin)
-	}
-	return 0
-}
+// make function to map nurse with plan
 
 func main() {
-	dsn := "top:1234@tcp(127.0.0.1:8889)/NursePlan?parseTime=true"
+	dsn := "sirawittop:1234@tcp(127.0.0.1:3306)/NursePlans?parseTime=true"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error connecting to the database:", err)
 	}
-
-	var plans []model.Plans
-	var planTypes []model.Plantypes
-	var hospitals []model.Hospitals
-	var wards []model.Wards
-	var nurses []model.Nurses
-	var OTs []model.OT
-
 	db.Find(&plans)
 	db.Find(&planTypes)
 	db.Find(&hospitals)
@@ -162,54 +160,73 @@ func main() {
 	db.Find(&nurses)
 	db.Find(&OTs)
 
-	planNumberMapping := mapPlanwithPlantype(plans, planTypes)
-	planWithType := generatePlanWithTypeMapping(plans, planNumberMapping)
-	var allNurseShifts [][]int
-	for i := 0; i < len(plans); i++ {
-		nurseshiftcal := calculateNursesShift(i, planWithType)
-		allNurseShifts = append(allNurseShifts, nurseshiftcal)
-	}
+	// planNumberMapping := mapPlanwithPlantype(plans, planTypes)
 
-	var nurseOT []int
-	for i := 0; i < len(plans); i++ {
-		nurseOT = append(nurseOT, calculateOT(i, planWithType, OTs, plans))
-	}
+	// planWithType := generatePlanWithTypeMapping(plans, planNumberMapping)
 
-	tmpl := template.Must(template.New("schedule.html").Funcs(template.FuncMap{
-		"splitPlanTypeName": splitPlanTypeName,
-	}).ParseFiles("templates/schedule.html"))
+	// nursename := mapNurseUser(nurses)
 
-	router := gin.Default()
-	router.SetHTMLTemplate(tmpl)
+	// Map nurses with their IDs
+	// nursename := mapNurseUser(nurses)
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "schedule.html", gin.H{
-			"Nurses":          nurses,
-			"PlanWithType":    planWithType,
-			"GetPlanWithType": getPlanWithTypeForNurse,
-			"CalNurseShifts":  allNurseShifts,
-			"nurseOT":         nurseOT,
-		})
-		db.Find(&plans)
-		db.Find(&planTypes)
-		db.Find(&hospitals)
-		db.Find(&wards)
-		db.Find(&nurses)
-		db.Find(&OTs)
+	// // Print the mapped nurses
+	// fmt.Println("Nurse Name Map:")
+	// for id, name := range nursename {
+	//     fmt.Printf("ID: %d, Name: %s\n", id, name)
+	// }
 
-		planNumberMapping := mapPlanwithPlantype(plans, planTypes)
-		planWithType = generatePlanWithTypeMapping(plans, planNumberMapping)
-		allNurseShifts = [][]int{}
-		for i := 0; i < len(plans); i++ {
-			nurseshiftcal := calculateNursesShift(i, planWithType)
-			allNurseShifts = append(allNurseShifts, nurseshiftcal)
-		}
+	// if planWithType[1][0] == "ช" {
+	// 	fmt.Println("yes")
+	// }
 
-		nurseOT = []int{}
-		for i := 0; i < len(plans); i++ {
-			nurseOT = append(nurseOT, calculateOT(i, planWithType, OTs, plans))
-		}
-	})
+	// var allNurseShifts [][]int
+	// for _, plan := range plans {
+	// 	nurseshiftcal := calculateNursesShift(planWithType[plan.Nurse_id])
+	// 	allNurseShifts = append(allNurseShifts, nurseshiftcal)
+	// }
+	// fmt.Println("allNurseShifts", allNurseShifts)
 
-	router.Run(":5555")
+	// fmt.Println("planwithtype", planWithType)
+	// var nurseOT []int
+	// for i := 0; i < len(plans); i++ {
+	// 	nurseOT = append(nurseOT, calculateOT(i, planWithType, OTs, plans))
+	// }
+
+	// tmpl := template.Must(template.New("schedule.html").Funcs(template.FuncMap{
+	// 	"splitPlanTypeName": splitPlanTypeName,
+	// }).ParseFiles("templates/schedule.html"))
+
+	// router := gin.Default()
+	// router.SetHTMLTemplate(tmpl)
+
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "schedule.html", gin.H{
+	// 		"Nurses":          nurses,
+	// 		"PlanWithType":    planWithType,
+	// 		"GetPlanWithType": getPlanWithTypeForNurse,
+	// 		"CalNurseShifts":  allNurseShifts,
+	// 		"nurseOT":         nurseOT,
+	// 	})
+	// 	db.Find(&plans)
+	// 	db.Find(&planTypes)
+	// 	db.Find(&hospitals)
+	// 	db.Find(&wards)
+	// 	db.Find(&nurses)
+	// 	db.Find(&OTs)
+
+	// 	planNumberMapping := mapPlanwithPlantype(plans, planTypes)
+	// 	planWithType = generatePlanWithTypeMapping(plans, planNumberMapping)
+	// 	allNurseShifts = [][]int{}
+	// 	for i := 0; i < len(plans); i++ {
+	// 		nurseshiftcal := calculateNursesShift(i, planWithType)
+	// 		allNurseShifts = append(allNurseShifts, nurseshiftcal)
+	// 	}
+
+	// 	nurseOT = []int{}
+	// 	for i := 0; i < len(plans); i++ {
+	// 		nurseOT = append(nurseOT, calculateOT(i, planWithType, OTs, plans))
+	// 	}
+	// })
+
+	// router.Run(":5555")
 }
--- model/model.go
+++ model/model.go
@@ -1,14 +1,84 @@
 package model
 
-import (
-	"time"
+import "gorm.io/gorm"
 
-	"github.com/shopspring/decimal"
-)
+type Hospitals struct {
+	gorm.Model
+	ID      uint `gorm:"primaryKey;autoIncrement;not null"`
+	Name    string
+	Address string
+	Phone   string
+}
+
+type Login struct {
+	gorm.Model
+	ID       uint `gorm:"primaryKey;autoIncrement;not null"`
+	Username string
+	Password string
+	RoleID   uint
+}
+
+type NurseLevel struct {
+	gorm.Model
+	ID    uint `gorm:"primaryKey;autoIncrement;not null"`
+	Level string
+}
+
+type OTs struct {
+	gorm.Model
+	ID           uint `gorm:"primaryKey;autoIncrement;not null"`
+	Name         string
+	Hospitals_id uint
+	Ward_id      uint
+	Morning      uint
+	Afternoon    uint
+	Night        uint
+}
+
+type Plans struct {
+	gorm.Model
+	ID           int `gorm:"primaryKey;autoIncrement;not null"`
+	Hospitals_id uint
+	Ward_id      uint
+	Nurse_id     uint
+	OT_id        uint
+	Day1         uint
+	Day2         uint
+	Day3         uint
+	Day4         uint
+	Day5         uint
+	Day6         uint
+	Day7         uint
+	Day8         uint
+	Day9         uint
+	Day10        uint
+	Day11        uint
+	Day12        uint
+	Day13        uint
+	Day14        uint
+	Day15        uint
+	Day16        uint
+	Day17        uint
+	Day18        uint
+	Day19        uint
+	Day20        uint
+	Day21        uint
+	Day22        uint
+	Day23        uint
+	Day24        uint
+	Day25        uint
+	Day26        uint
+	Day27        uint
+	Day28        uint
+	Day29        uint
+	Day30        uint
+	Day31        uint
+}
 
 type Plantypes struct {
-	ID         int
-	TypeName   string
+	gorm.Model
+	ID         int `gorm:"primaryKey;autoIncrement;not null"`
+	Type       string
 	Morning    bool
 	Afternoon  bool
 	Night      bool
@@ -17,102 +87,38 @@ type Plantypes struct {
 	Nleave     bool
 	C          bool
 	Part       bool
-	OtM        bool
-	OtA        bool
-	OtN        bool
+	Otm        bool
+	Ota        bool
+	Otn        bool
 	StatusS    bool
-	StatusOt12 bool
-	StatusOt8  bool
+	StatusOT8  bool
+	StatusOT12 bool
 }
 
-type Hospitals struct {
-	ID      int
-	Name    string
-	Address string
-	Phone   string
+type Roles struct {
+	gorm.Model
+	ID   uint `gorm:"primaryKey;autoIncrement;not null"`
+	Name string
+}
+
+type User struct {
+	gorm.Model
+	ID              uint `gorm:"primaryKey;autoIncrement;not null"`
+	Hospital_id     uint
+	Ward_id         uint
+	Login_id        uint
+	Nurse_level     uint
+	Firstname       string
+	Lastname        string
+	Gender          string
+	Phone           string
+	Email           string
+	Employment_year string
 }
 
 type Wards struct {
-	Id         int
-	Name       string
-	HospitalID int
-}
-
-type Nurses struct {
-	ID                        int
-	MedicalP_ID               int
-	CitizenI_Num              int
-	ProfessionalM_license_Num string
-	Firstname                 string
-	Lastname                  string
-	Birthday                  time.Time
-	Position                  string
-	WardID                    int
-	Employment_Year           int
-	HospitalID                int
-	Salary                    decimal.Decimal
-	Gender                    string
-	Phone_Number              string
-	Number_Children           int
-	MaritalS_ID               int
-	Religion                  string
-	Email                     string
-	Line_ID                   string
-	RelativeP_Num             string
-	Village                   string
-	House_number              string
-	Zip_Code                  string
-	Province                  string
-	District                  string
-	Subdistrict               string
-}
-
-type Plans struct {
-	Id         int
-	HospitalID int
-	WardID     int
-	Month      int
-	NurseID    int
-	Day1       int
-	Day2       int
-	Day3       int
-	Day4       int
-	Day5       int
-	Day6       int
-	Day7       int
-	Day8       int
-	Day9       int
-	Day10      int
-	Day11      int
-	Day12      int
-	Day13      int
-	Day14      int
-	Day15      int
-	Day16      int
-	Day17      int
-	Day18      int
-	Day19      int
-	Day20      int
-	Day21      int
-	Day22      int
-	Day23      int
-	Day24      int
-	Day25      int
-	Day26      int
-	Day27      int
-	Day28      int
-	Day29      int
-	Day30      int
-	Day31      int
-	OT         int
-}
-
-type OT struct {
-	Id         int
-	Name       string
-	HospitalID int
-	WardID     int
-	Morning    int
-	Afternoon  int
-	Night      int
+	gorm.Model
+	ID           uint `gorm:"primaryKey;autoIncrement;not null"`
+	Hospitals_id uint
+	Name         string
 }
